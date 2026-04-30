aaad9628e1537bcf19c320919eb8f35932727502
Destinygerald <118162858+Destinygerald@users.noreply.github.com>
2026-04-30 14:25:26 +0100

    Comment out app import and initialization
    Comment out the import of app from server.js and the app initialization.

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-import app from "../server.js";
+//import app from "../server.js";
 import serverless from "serverless-http";
 
 import express from "express";
 
-// const app = express();
+/const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello from Vercel");
