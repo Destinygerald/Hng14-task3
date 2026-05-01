9aebf8371bd69dc3058f4bf689817c8423988bb5
Destinygerald <destinygerald85@gmail.com>
2026-05-01 01:33:54 +0100

    fix: cleaning up

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-// import http from "http";
 import dotenv from "dotenv";
-import { app } from "./server";
+import { app } from "./server.js";
 import ServerlessHttp from "serverless-http";
-// import { logger } from "./utils/logger.js";
-// import { DbSeeding } from "./config/db.js";
 dotenv.config();
-
-// const PORT = process.env.PORT || 8000;
-
-// const server = http.createServer(app);
-
-// process.on("unhandledRejection", (reason, promise) => {
-//   // logger.error("Server Failed:", promise, "reason: ", reason);
-//   console.error(`Server Failed: ${reason}`);
-//   return;
-// });
-
-// server.listen(PORT, async () => {
-//   // await DbSeeding();
-//   console.log("Listening on PORT " + PORT);
-// });
 
 export default ServerlessHttp(app);
