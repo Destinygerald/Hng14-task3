cd6b37272755049377c9f46bd913bebb81ceaeb4
Destinygerald <destinygerald85@gmail.com>
2026-05-01 02:37:35 +0100

    fix: swagger - added route paths correction

--- config/swagger.js
+++ config/swagger.js
@@ -1,6 +1,11 @@
 import swaggerJSDoc from "swagger-jsdoc";
 import path from "node:path";
 
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const options = {
   definition: {
     openapi: "3.0.0",
